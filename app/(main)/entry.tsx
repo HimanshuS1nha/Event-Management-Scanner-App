91be66b1d6993b28f8f31302b952fe3c65401c24
Himanshu Sinha <ashusinha1904@gmail.com>
2024-07-06 15:48:08 +0530

    entry page UI complete

--- app/(main)/entry.tsx
+++ app/(main)/entry.tsx
@@ -1,11 +1,47 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, Pressable } from "react-native";
+import React, { useState, useCallback } from "react";
+import tw from "twrnc";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useIsFocused } from "@react-navigation/native";
+import { Camera, CameraView } from "expo-camera";
+
+import SafeView from "@/components/SafeView";
+import LoadingModal from "@/components/LoadingModal";
 
 const Entry = () => {
+  const isFocused = useIsFocused();
+
+  const [scanned, setScanned] = useState(false);
+
+  const handleLogout = useCallback(async () => {}, []);
   return (
-    <View>
-      <Text>Entry</Text>
-    </View>
+    <SafeView style={tw`justify-center`}>
+      <View style={tw`absolute w-full items-end top-[8%] right-3`}>
+        <Pressable onPress={handleLogout}>
+          <MaterialCommunityIcons name="logout" size={27} color="white" />
+        </Pressable>
+      </View>
+
+      <View style={tw`items-center mb-5`}>
+        <Text style={tw`text-white font-semibold text-lg`}>
+          {scanned ? "Not scanning" : "Scanning..."}
+        </Text>
+      </View>
+
+      {isFocused && <CameraView style={tw`h-[70%]`} facing="back"></CameraView>}
+
+      <View style={tw`items-center mt-6`}>
+        {scanned && (
+          <Pressable
+            onPress={() => setScanned(false)}
+            style={tw`items-center py-3 bg-blue-600 px-6 rounded-full`}
+            disabled={!scanned}
+          >
+            <Text style={tw`text-white text-base font-medium`}>Scan Again</Text>
+          </Pressable>
+        )}
+      </View>
+    </SafeView>
   );
 };
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,31 +1,62 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  Image,
+  ActivityIndicator,
+  Alert,
+  BackHandler,
+  Linking,
+} from "react-native";
 import { useEffect } from "react";
 import { router, useRootNavigationState } from "expo-router";
+import { useCameraPermissions } from "expo-camera";
+import tw from "twrnc";
 
+import SafeView from "@/components/SafeView";
 import { useUser } from "@/hooks/useUser";
 
 export default function Index() {
   const rootNavigationState = useRootNavigationState();
   const { isLoggedIn } = useUser();
+  const [permission, requestPermission] = useCameraPermissions();
 
   useEffect(() => {
     if (rootNavigationState?.key) {
       if (isLoggedIn) {
-        router.replace("/entry");
+        if (permission?.granted) {
+          router.replace("/entry");
+        } else if (permission?.canAskAgain) {
+          requestPermission();
+        } else {
+          Alert.alert(
+            "Error",
+            "This app needs the camera permission to operate",
+            [
+              {
+                text: "Cancel",
+                onPress: BackHandler.exitApp,
+              },
+              {
+                text: "Open Settings",
+                onPress: Linking.openSettings,
+              },
+            ]
+          );
+        }
       } else {
-        router.replace("/login");
       }
     }
-  }, [rootNavigationState?.key]);
+  }, [rootNavigationState?.key, permission?.granted]);
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeView style={tw`justify-center items-center gap-y-7`}>
+      <Image
+        source={require("../assets/images/logo.webp")}
+        style={tw`w-32 h-32 rounded-full`}
+      />
+      <Text style={tw`text-white text-2xl font-medium`}>
+        Event Management Head App
+      </Text>
+
+      <ActivityIndicator size={45} color={"violet"} />
+    </SafeView>
   );
 }
